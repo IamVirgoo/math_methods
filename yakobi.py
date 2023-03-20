c242a8019314f868b1a3f8f52e82fba82e948a13
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-03-20 19:25:53 +0300

    Initial commit

---
+++ yakobi.py
@@ -0,0 +1,41 @@
+import copy
+
+import numpy as np
+
+
+A = [
+    [0.11, 0.89, 0],
+    [0.84, 12.57, 1.54],
+    [1.27, 0.05, 8.07],
+]
+
+B = [
+    0.06,
+    3.15,
+    1.89
+]
+
+
+def yacobi(A, B, eps):
+    n = len(A)
+    D = np.eye(n)
+    F = copy.deepcopy(A)
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                F[i][j] = 0
+                D[i][j] = A[i][j]
+    D_inv = np.linalg.inv(D)
+    C = np.dot(-D_inv, F)
+    d = np.dot(D_inv, B)
+    X = [0] * n
+    r = nevyazka(A, B, X)
+    while r > eps:
+        X = np.dot(C, X) + d
+        r = nevyazka(A, B, X)
+    print("Метод Якоби.")
+    print("X:", X)
+    print("epsilon =", eps)
+
+
+yacobi(A, B, 0.001)
